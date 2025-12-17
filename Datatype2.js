b4a3b56bbb2de2379d6c8e0ef46a776204541010
tejaswi3126 <tejaswibharath3129@gmail.com>
2025-12-17 17:30:48 +0530

    Add files via upload
    Day1

---
+++ Datatype2.js
@@ -0,0 +1,10 @@
+var firstName="Tejaswi"
+var companyName="Testleaf"
+var mobileNumber=1234567892
+var isAutomation=true
+var hasPlaywrite
+console.log("TesterName:" +firstName+ " " + typeof (firstName))
+console.log("OrganizationNmae:" +companyName+ " " + typeof (companyName))
+console.log("ContactNumber:" +mobileNumber+ " " + typeof (mobileNumber))
+console.log("IsAutomationtester:" +isAutomation+ " " + typeof (isAutomation))
+console.log("HasPlaywritrExperience:" +hasPlaywrite+ " " + typeof (hasPlaywrite))
